8b7a7308cd026f94ad0e0fcdc7fa8adb285caa79
Toregeldi480 <toregelditolembai@proton.me>
2026-06-22 11:16:17 +0500

    fixed clean.sh volume namings

--- clean.sh
+++ clean.sh
@@ -1,1 +1,5 @@
-docker compose down && docker volume rm wetube_kafka-data && docker volume rm wetube_zookeeper-data && docker volume rm wetube_postgres-data && docker volume rm wetube_video-storage
+docker compose down && 
+docker volume rm spring-tutorial-wetube_kafka-data && 
+docker volume rm spring-tutorial-wetube_zookeeper-data && 
+docker volume rm spring-tutorial-wetube_postgres-data && 
+docker volume rm spring-tutorial-wetube_video-storage
